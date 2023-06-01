e04d3f80ea6b13bde3111bfd1b082775befe9ae6
Emanuele Lucchese MSI <lucchese.emanuele.el@gmail.com>
2023-06-01 14:06:11 +0200

    Spostamento codice in altra folder

--- api/whatsapp_chats_api_v3/resources/js/app.js
+++
@@ -1,9 +0,0 @@
-require('./bootstrap');
-import { createApp } from 'vue'
-import Home from './components/Home'
-
-const app = createApp({})
-
-app.component('welcome', Home)
-
-app.mount('#app')
--- api/whatsapp_chats_api_v3/resources/views/messages/message_from.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="row messagge_from" v-for="message in messages_from">
-    <div class="card card_from" style="width: 18rem;">
-        <img v-if="message.image" class="card-img-top" :src="'data:image/jpeg;base64,'+message.image" style="width: 250px !important;">
-        <div class="card-body">
-            <p class="card-text">@{{ message.text }}</p>
-            <small class="info-data-me">@{{ message.data }}</small>
-        </div>
-    </div>
-</div>
